b714427fca4529a9938e33882b27839f8eac6a4f
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-08-18 22:13:04 +0700

    Add hover trail effect

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ for (let i = 0; i < 4; i++) {
   for (let j = 0; j < 4; j++) {
     const grid = document.createElement("div");
     grid.classList.add("grid");
-    grid.innerHTML = `${i}, ${j}`;
     gridContainer.appendChild(grid);
   }
   body.appendChild(gridContainer);
