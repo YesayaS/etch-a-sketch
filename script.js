2ecc3c8af62388eff57cbecf6a1af30cda54eafc
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-08-19 00:03:13 +0700

    Add basic UI
    add basic UI, popup feature not fully function yet

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-const body = document.body;
-for (let i = 0; i < 4; i++) {
-  const gridContainer = document.createElement("div");
-  gridContainer.classList.add("grid-container");
-  for (let j = 0; j < 4; j++) {
-    const grid = document.createElement("div");
-    grid.classList.add("grid");
-    gridContainer.appendChild(grid);
+createGrid(4);
+function createGrid(size) {
+  const grids = document.querySelector(".grids");
+  grids.innerHTML = "";
+  for (let i = 0; i < size; i++) {
+    const gridContainer = document.createElement("div");
+    gridContainer.classList.add("grid-container");
+    for (let j = 0; j < size; j++) {
+      const grid = document.createElement("div");
+      grid.classList.add("grid");
+      gridContainer.appendChild(grid);
+    }
+    grids.appendChild(gridContainer);
   }
-  body.appendChild(gridContainer);
+}
+
+document.querySelector(".grid-size").addEventListener("click", showPopUp);
+document
+  .querySelector(".change-size")
+  .addEventListener("click", changeGridSize);
+
+function showPopUp() {}
+
+function changeGridSize() {
+  const newSize = document.querySelector("#new-size").value;
+  if (newSize !== "") {
+    createGrid(newSize);
+  }
 }
